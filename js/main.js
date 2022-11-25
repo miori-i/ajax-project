630f4de55806aa5540f4e3cccb99783343d326c7
Miori Imai <imamio1227@gmail.com>
2022-11-25 17:57:09 +0000

    Listen for clicks on the parent element of all rendered recipes in all recipes view. #2

--- js/main.js
+++ js/main.js
@@ -52,11 +52,23 @@ xhrLunch.addEventListener('load', function () {
           for (var i = 0; i < xhrDinner.response.hits.length; i++) {
             allRecipes.push(xhrDinner.response.hits[i]);
           }
+          // console.log('array:', allRecipes);
 
           // Use a loop to create a DOM tree for each recipe in the data model and append it to the page
           for (var k = 0; k < allRecipes.length; k++) {
             var $containerForRecipes = document.querySelector('.container-for-recipes');
             $containerForRecipes.appendChild(renderRecipe(allRecipes[k]));
+          }
+
+          // Listen for clicks on the parent element of all rendered recipes in all recipes view
+
+          $containerForRecipes.addEventListener('click', viewRecipeDetails);
+
+          function viewRecipeDetails(event) {
+            // console.log('cliked elemtnt:', event.target);
+
+            // for (var i = 0; i < allRecipes)
+
           }
 
         });
@@ -73,32 +85,39 @@ xhrLunch.send();
 function renderRecipe(object) {
   var $recipeWrapper = document.createElement('div');
   $recipeWrapper.setAttribute('class', 'recipe-wrapper');
+  $recipeWrapper.setAttribute('name', object.recipe.label);
 
   // row 1: image
   var $row1 = document.createElement('div');
   $row1.setAttribute('class', 'row recipe-image-wrapper');
+  $row1.setAttribute('name', object.recipe.label);
   $recipeWrapper.appendChild($row1);
 
   var $columnFull1 = document.createElement('div');
   $columnFull1.setAttribute('class', 'column-full');
+  $columnFull1.setAttribute('name', object.recipe.label);
   $row1.appendChild($columnFull1);
 
   var $image = document.createElement('img');
   $image.setAttribute('src', object.recipe.images.REGULAR.url);
-  $image.setAttribute('alt', 'a picture of a recipe');
+  $image.setAttribute('alt', 'a pic of ' + object.recipe.label);
+  $image.setAttribute('name', object.recipe.label);
   $columnFull1.appendChild($image);
 
   // row 2: recipe name
   var $row2 = document.createElement('div');
   $row2.setAttribute('class', 'row recipe-text-wrapper');
+  $row2.setAttribute('name', object.recipe.label);
   $recipeWrapper.appendChild($row2);
 
   var $columnFull2 = document.createElement('div');
   $columnFull2.setAttribute('class', 'column-full');
+  $columnFull2.setAttribute('name', object.recipe.label);
   $row2.appendChild($columnFull2);
 
   var $recipeName = document.createElement('p');
   $recipeName.setAttribute('class', 'recipe-name');
+  $recipeName.setAttribute('name', object.recipe.label);
   $recipeName.textContent = object.recipe.label;
   $columnFull2.appendChild($recipeName);
   // resize($recipeName, $columnFull2);
@@ -106,50 +125,61 @@ function renderRecipe(object) {
   // row 3: recipe tiem & calories
   var $row3 = document.createElement('div');
   $row3.setAttribute('class', 'row recipe-text-wrapper');
+  $row3.setAttribute('name', object.recipe.label);
   $recipeWrapper.appendChild($row3);
 
   var $columnHalf1 = document.createElement('div');
   $columnHalf1.setAttribute('class', 'column-half timer-icon-wrapper');
+  $columnHalf1.setAttribute('name', object.recipe.label);
   $row3.appendChild($columnHalf1);
 
   var $recipeTime = document.createElement('p');
   $recipeTime.setAttribute('class', 'recipe-time');
+  $recipeTime.setAttribute('name', object.recipe.label);
   $recipeTime.textContent = object.recipe.totalTime + ' min';
   $columnHalf1.appendChild($recipeTime);
 
   var $timerIcon = document.createElement('i');
   $timerIcon.setAttribute('class', 'fa-regular fa-clock fa-sm');
+  $timerIcon.setAttribute('name', object.recipe.label);
   $columnHalf1.appendChild($timerIcon);
 
   var $columnHalf2 = document.createElement('div');
   $columnHalf2.setAttribute('class', 'column-half');
+  $columnHalf2.setAttribute('name', object.recipe.label);
   $row3.appendChild($columnHalf2);
 
   var $recipeCalories = document.createElement('p');
   $recipeCalories.setAttribute('class', 'recipe-calories');
+  $recipeCalories.setAttribute('name', object.recipe.label);
   $recipeCalories.textContent = Math.round(object.recipe.calories) + ' calories';
   $columnHalf2.appendChild($recipeCalories);
 
   // row 4: number of recipe ingredients & type of cousine
   var $row4 = document.createElement('div');
   $row4.setAttribute('class', 'row recipe-text-wrapper');
+  $row4.setAttribute('name', object.recipe.label);
   $recipeWrapper.appendChild($row4);
 
   var $columnHalf3 = document.createElement('div');
   $columnHalf3.setAttribute('class', 'column-half');
+  $columnHalf3.setAttribute('name', object.recipe.label);
   $row4.appendChild($columnHalf3);
 
   var $recipeIngredients = document.createElement('p');
   $recipeIngredients.setAttribute('class', 'recipe-number-of-ingredients');
+  $recipeIngredients.setAttribute('name', object.recipe.label);
   $recipeIngredients.textContent = object.recipe.ingredients.length + ' ingredients';
   $columnHalf3.appendChild($recipeIngredients);
 
   var $columnHalf4 = document.createElement('div');
   $columnHalf4.setAttribute('class', 'column-half');
+  $columnHalf4.setAttribute('name', object.recipe.label);
   $row4.appendChild($columnHalf4);
 
   var $recipeCousine = document.createElement('p');
   $recipeCousine.setAttribute('class', 'recipe-cousine');
+  $recipeCousine.setAttribute('name', object.recipe.label);
   $recipeCousine.textContent = object.recipe.cuisineType;
   $recipeCousine.textContent = $recipeCousine.textContent.charAt(0).toUpperCase() + $recipeCousine.textContent.slice(1);
   $columnHalf4.appendChild($recipeCousine);
